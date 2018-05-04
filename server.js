721c77133aa34c1baaa68959ff4ceb25670c62dc
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-04 16:31:23 -0500

    change to listen to environmental variable PORT

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 
 app.post('/imageurl', (req, res) => {image.handleAPICall(req, res)});
 
-app.listen(3000, () => {
-  console.log('app running in port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app running in port ${process.env.PORT}`);
 });
