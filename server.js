11ae82229beddf73fb0e67a8291022692079b14d
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-04 16:51:47 -0500

    Change database

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-      host: '127.0.0.1',
-      user: 'emmanuel',
-      password: '',
-      database: 'smart-brain'
+      host: process.env.DATABASE_URL,
+      ssl: true
   }
 });
 
